4e31223024d9c8146bfd1ce5d9b5bc291f1c6cb6
Zielony Buszmen <Zielony Buszmen@ZielonyBuszmen>
2015-10-19 19:06:09 +0200

    Dodanie funkcji przyciskow i zaprezentowanie menu z lewej

--- funkcje/f_przyciski.php
+++ funkcje/f_przyciski.php
@@ -2,5 +2,19 @@
     //plik zawiera funkcje tworzące "inteligentne" przyciski.
     //inteligentny przycisk to taki, który jest zaznaczony, jeśli został kliknięty
 
-    // work in progrss
+function standardowy_przycisk($adres, $tresc)
+{
+    // sprawdzamy czy w adresie ($_SERVER) znajduje się ciąg z adresu przycisku ($adres)
+    // jeśli tak, to dodajemy klasę .active
+    // w przeciwnym wypadku nic nie zmieniamy i wyświetlamy bez podświetlenia
+    if( strpos( $_SERVER['REQUEST_URI'], $adres) )
+    {
+        echo '<li class="active"><a href="'.$adres.'">'.$tresc.'</a></li>';
+    }
+    else
+    {
+        echo '<li><a href="'.$adres.'">'.$tresc.'</a></li>';
+    }  
+}
+
 ?>
