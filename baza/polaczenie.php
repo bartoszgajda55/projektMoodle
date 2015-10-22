9b6fa59b6dc97c87f76da311be76066aae519614
Admin <Admin@PC>
2015-10-22 21:08:02 +0200

    Nic ciekawego 2

--- baza/polaczenie.php
+++ baza/polaczenie.php
@@ -4,7 +4,7 @@
 
     $adres = '127.0.0.1';
     $uzytkownik = 'root';
-    $haslo = '';
+    $haslo = 'bazydanych';
     $nazwa_bazy = 'projektMoodle';
 
     // ustanawiamy połączenie z bazą, jak się nie łączy, wyświetlamy błąd
