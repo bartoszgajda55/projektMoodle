cf723b1914d6587da5ca7fa2ce2b7afc95cbd588
Zielony Buszmen <Zielony Buszmen@ZielonyBuszmen>
2015-10-23 20:42:20 +0200

    Dodanie wyświetlania danego kursu, listy lekcji i danej lekcji

--- tresc/panele_userow/uczen/u_info.php
+++ tresc/panele_userow/uczen/u_info.php
@@ -2,15 +2,23 @@
 if (!zalogowano()) return;
 ?>
 
-Tabelka z informacjami o userze
+<h3>Szczegółowe informacje o użytkowniku</h3>
+
 <hr>
 
+<p>Jesteś zalogowany jako: <b><?=$_SESSION['imie']?> <?=$_SESSION['nazwisko']?></b></p>
+<p>Login: <b><?=$_SESSION['login']?></b></p>
+<p>Mail: <b></b></p>
+<p>Ranga: <b><?=$_SESSION['typ']?></b></p>
+
+
 <?php
+/*
 echo "zalogowany - <b>".$_SESSION['zalogowany']."</b><br>";
 echo "login - <b>".$_SESSION['login']."</b><br>";
 echo "typ - <b>".$_SESSION['typ']."</b><br>";
 echo "id_usera - <b>".$_SESSION['id_usera']."</b><br>";
 echo "imie - <b>".$_SESSION['imie']."</b><br>";
 echo "nazwisko - <b>".$_SESSION['nazwisko']."</b><br>";
-//echo "".."<br>";
+echo "".."<br>";*/
 ?>
---
+++ tresc/u_kursy/lista_lekcji.php
@@ -0,0 +1,73 @@
+<div class="col-md-2"></div>
+<div class="col-md-8">
+<?php
+// plik wyświetla listę lekcji w danym kursie.
+// Po kliknięciu na daną lekcję można przejść do niej
+
+    // sprawdzamy, czy istenieje $_GET[id]
+    // zmienna ta przechowuje id kursu, który ma być wyświetlony
+    // jeśli tej zmiennej nie ma to możemy od razu darować sobie wszystko i nic nie wyświetlać
+    if (!isset($_GET['id']))
+    {
+        echo "Nie podano zmiennej ID";
+        return;
+    }
+    
+    // następnie tworzymy kilka zmiennych globalnych i przypisujemy do nich domyślne wartości
+    // niektóre wartości zmiennych będą zmieniane podczas trwania skryptu
+    $id = $_GET['id'];
+    $nazwa_kursu = "Brak nazwy kursu";
+    
+    // sprawdzenie czy użytkownik należy do kursu. Jeśli nie, nic nie wyświetlamy
+    if(!zalogowano()) return;
+    if (zalogowano())
+    {   
+        // jeśli to zapytanie da 0 wyników, znaczy to tyle że usera nie ma w tym kursie
+        $wynik = mysql_query("SELECT * FROM `zapisy` WHERE id_uzytkownika={$_SESSION['id_usera']}");
+        if(mysql_num_rows($wynik) <=0) 
+        {
+            echo "Nie należysz do tego kursu!";
+            return;
+        }
+    }
+    
+    // jeśli skrypt doszedł tutaj, znaczy że użytkownik moze przeglądać ten kurs, bo do niego należy
+    // pobranie nazwy kursu i przypisanie jej do zmiennej $nazwa_kursu
+    $wynik_kurs_nazwa = mysql_query("SELECT * FROM `kursy` WHERE id_kursu={$id}");
+    while ($nazwa_kursu_row = mysql_fetch_assoc($wynik_kurs_nazwa)) 
+    {
+        $nazwa_kursu = $nazwa_kursu_row['nazwa'];
+    }
+?>
+    <h3><a href="?v=tresc/u_kursy/lista_kursow_uzytkownika"><?=$nazwa_kursu?></a></h3>
+    Lista lekcji w kursie
+    <hr>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Nr. lekcji</th>
+                <th>Temat</th>
+                <th></th>
+             </tr>
+        </thead>
+        <tbody>
+<?php
+    $wynik_kurs_nazwa = mysql_query("SELECT * FROM `lekcje` WHERE id_kursu={$id}");
+    $nr = 0;
+    while ($r = mysql_fetch_assoc($wynik_kurs_nazwa)) 
+    {
+        $nr++; // licznik lekcji
+        // stworzenie linku
+        $link = "?v=tresc/u_kursy/dana_lekcja&id={$r['id_lekcji']}&id_kursu={$id}";
+        // tabela z listą lekcji
+        echo '<tr>';
+        echo '<td>'.$nr.'</td>';
+        echo '<td><a href="'.$link.'">'.$r['temat'].'</a></td>';
+        echo '<td><a class="btn btn-default btn-sm" href="'.$link.'" role="button">Przejdź do lekcji</a></td>';
+        echo '</tr>'; 
+    }  
+?>
+        </tbody>
+    </table>
+</div>
+<div class="col-md-2"></div>
